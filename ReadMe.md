a65abbe538fd879206ce017ad2931a391160babc
孙小涛 <sunxiaotao@admaster.com.cn>
2020-03-20 07:51:09 +0800

    feat: father 实时更新

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,11 @@
 ## Lerna 管理多项目
+
+### st-components 的实时编译和项目的实时更新
+
+当 st-components 内容更新时，`father` 通过 `watch` 可以实时编译，项目可以获取最新的内容。
+
+实验后，可以实时更新的项目:
+
+- 自定义的 webpack
+- CRA 项目
+- UMI 项目--待完成
--- packages/st-components/src/components/MiniButton/index.tsx
+++ packages/st-components/src/components/MiniButton/index.tsx
@@ -6,7 +6,7 @@ interface MinuButtonProps {
 const MinuButton: FC<MinuButtonProps> = ({ name = 'test' }) => {
   return (
     <div>
-      <h4>Button: {name}</h4>
+      <h4>Button-Change11: {name}</h4>
     </div>
   );
 };
