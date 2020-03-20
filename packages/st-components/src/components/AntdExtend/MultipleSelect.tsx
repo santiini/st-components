892cc949014d71a57aade6a01ad24960654c8ce4
孙小涛 <sunxiaotao@admaster.com.cn>
2020-03-20 14:57:32 +0800

    feat: @st/st-components 添加

---
+++ packages/st-components/src/components/AntdExtend/MultipleSelect.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Select } from 'antd';
+import { SelectProps } from 'antd/lib/select';
+import { SelectValue } from 'antd/lib/tree-select';
+
+class MultipleSelect extends React.Component<SelectProps<SelectValue>> {
+  static Option = Select.Option;
+
+  /* 过滤 select */
+  onFilterChildren: SelectProps<SelectValue>['filterOption'] = (inputValue, option) => {
+    const inputUpper = inputValue.toUpperCase();
+    if (!option?.children) return false;
+    return (
+      option.children
+        .toString()
+        .toUpperCase()
+        .indexOf(inputUpper) > -1
+    );
+  };
+
+  render() {
+    const { children, ...restProps } = this.props;
+    return (
+      <Select showSearch allowClear mode="multiple" filterOption={this.onFilterChildren} {...restProps}>
+        {children}
+      </Select>
+    );
+  }
+}
+
+export default MultipleSelect;
--- packages/st-components/src/index.ts
+++ packages/st-components/src/index.ts
@@ -3,3 +3,5 @@
  */
 export { default as MiniButton } from './components/MiniButton';
 export { default as MiniBox } from './components/MiniBox';
+export { default as ExtendSelect } from './components/AntdExtend/ExtendSelect';
+export { default as MultipleSelect } from './components/AntdExtend/MultipleSelect';
